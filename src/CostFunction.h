1a6fe4e0efa598c8db6d07c28c66af1c5b146b84
Pier <pier.tirindelli@gmail.com>
2015-10-06 11:14:06 +0200

    CostFunction.h

---
+++ src/CostFunction.h
@@ -0,0 +1,48 @@
+#ifndef COSTFUNCTION_H
+#define COSTFUNCTION_H
+
+
+class MyCostFunctor{
+
+    public:
+
+        MyCostFunctor(std::vector<double> sat_i_, double t_i_, double vel_)
+            : sat_i(sat_i_), t_i(t_i_), vel(vel_) {}
+
+        template <typename T>
+        bool operator()(const T* const pos, const T* const bias, T* residual) const {
+
+            T square_sum = T(0);
+
+            for (int i = 0; i < sat_i.size(); ++i) {
+                square_sum += pow(pos[i] - T(sat_i[i]) , 2);
+            }
+
+            T distance = (square_sum != T(0)) ? sqrt(square_sum) : T(0) ;
+
+            residual[0] = distance + vel * (bias[0] - t_i);
+
+
+            /* TODO
+             * operare con misure di tempo
+             *
+             *
+             * error = expected measurement - actual measurement
+             * con  expected measurement = distance / vel + bias[0]
+             *      actual measurement   = t_i
+             */
+
+ //           residual[0] = distance / vel + bias[0] - t_i;
+
+            return true;
+        }
+
+
+    private:
+        const std::vector<double> sat_i;
+        const double t_i;
+        const double vel;
+};
+
+
+#endif // COSTFUNCTION_H
